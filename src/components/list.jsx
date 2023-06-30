1f24d51dfe70b5f1ee39653cb68103a44ad19394
Brian Kimurgor <kimurgorbrian20@gmail.com>
2023-06-30 16:10:14 +0300

    react skeleton

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,5 +1,18 @@
 var react = require ('react');
-var listItem = require('./listitem');
+var listItem = require('./listitem.jsx');
 
 
-var ingredient = [{"id"}]
+var ingredient = [{"id":1,"text":"ham"},{"id":2,"text": "cheese"}, {"id":3, "text": "peperoni"}]
+
+var List = React.createClass({
+    render: function(){
+        var listItems = ingredient.map(function(item){
+            return <listitem key = {items.id} ingredient ={item.text}/>;
+        })
+
+        return (<ul>{listItems}</ul>)
+    }
+});
+
+
+module.exports = List;
